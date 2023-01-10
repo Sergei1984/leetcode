a0bceb0eecdf19332884586c71c5d48bc494c37c
Serhii Tokariev <seregat1984@gmail.com>
2023-01-10 17:03:39 +0200

    10. Regex matching optimized.

--- src/p010_hard_regular_expression_matching_optimized.rs
+++ src/p010_hard_regular_expression_matching_optimized.rs
@@ -4,13 +4,89 @@ pub struct Solution;
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
         let input = s.as_bytes();
-        let pattern = Self::parse_regex(input);
+        let pattern = Self::parse_regex(p.as_bytes());
 
         return Self::match_recursive(input, pattern);
     }
 
     pub fn match_recursive(input: &[u8], pattern: Option<Box<PatternPair>>) -> bool {
-        false
+        let _input_str = String::from_utf8(input.to_vec()).unwrap();
+        let _fixed_str = pattern
+            .as_ref()
+            .map(|p| p.fixed.map(|f| String::from_utf8(f.to_vec()).unwrap()))
+            .flatten();
+        let _repeatable_str = pattern
+            .as_ref()
+            .map(|p| p.repeatable.map(|f| String::from_utf8(f.to_vec()).unwrap()))
+            .flatten();
+
+        if let Some(p) = pattern {
+            if let Some(fixed) = p.fixed {
+                if let Some(repeatable) = p.repeatable {
+                    let mut fixed_match_index = 0;
+
+                    while let Some(idx) = Self::index_of_pattern(input, fixed, fixed_match_index) {
+                        let repeatable_part = &input[0..idx];
+
+                        if Self::match_repeatable(repeatable_part, repeatable) {
+                            let rest_matches = Self::match_recursive(
+                                &input[idx + fixed.len()..],
+                                p.next_pattern.clone(),
+                            );
+
+                            if rest_matches {
+                                return true;
+                            }
+                        }
+
+                        fixed_match_index = idx + 1;
+                    }
+
+                    return false;
+                } else {
+                    // if there is no repeatable part, fixed part must match the start of string
+                    let index = Self::index_of_pattern(input, fixed, 0);
+
+                    let matches_start = index.map(|v| v == 0).unwrap_or(false);
+                    if !matches_start || fixed.len() > input.len() {
+                        // input has some symbols before fixed part
+                        return false;
+                    }
+
+                    return Self::match_recursive(&input[fixed.len()..], p.next_pattern);
+                }
+            } else {
+                return Self::match_repeatable(input, p.repeatable.unwrap()); // if there is no fixed part pattern matches if repeatable part matches full input
+            }
+        } else {
+            return input.len() == 0; // must be end of string on end of pattern
+        }
+    }
+
+    // Check if input fully matched by the pattern or pattern prefix
+    pub fn match_repeatable(input: &[u8], pattern: &[u8]) -> bool {
+        if input.len() == 0 {
+            return true;
+        }
+
+        if pattern.len() == 0 {
+            return false;
+        }
+
+        let match_symbol = pattern[0];
+
+        if match_symbol == b'.' {
+            return true;
+        }
+
+        for i in 0..input.len() {
+            let c = input[i];
+            if c != match_symbol {
+                return Self::match_repeatable(&input[i..], &pattern[2..]);
+            }
+        }
+
+        true
     }
 
     pub fn parse_regex<'a>(regex: &'a [u8]) -> Option<Box<PatternPair<'a>>> {
@@ -50,41 +126,47 @@ impl Solution {
 
     // Captures repeatable part of regex (first tuple member) and returns rest of slice (second tuple member)
     pub fn capture_repeatable<'a>(regex: &'a [u8]) -> (Option<&'a [u8]>, &'a [u8]) {
-        if regex.len() == 0 {
+        if regex.len() < 2 {
             return (None, regex);
         }
-        for i in 0..regex.len() {
-            if regex[i] == b'*' {
-                continue;
-            }
 
-            let next = regex.get(i + 1);
-            if let Some(next_char) = next {
-                if *next_char != b'*' {
-                    if i < 2 {
-                        return (None, regex); // No repeatable part
-                    } else {
-                        return (Some(&regex[0..i]), &regex[i..]);
-                    }
+        let mut i = 1;
+        while i < regex.len() {
+            // a*b*cd
+            if regex[i] != b'*' {
+                let repeatable_end = i - 1;
+                if repeatable_end == 0 {
+                    return (None, regex);
+                } else {
+                    return (Some(&regex[0..repeatable_end]), &regex[repeatable_end..]);
                 }
             }
+
+            i = i + 2;
+        }
+        if regex.len() % 2 == 1 {
+            return (Some(&regex[0..regex.len() - 1]), &regex[regex.len() - 1..]);
         }
 
         return (Some(&regex), &regex[regex.len()..]);
     }
 
-    pub fn index_of(input: &[u8], pattern: &[u8]) -> Option<usize> {
+    pub fn index_of_pattern(input: &[u8], pattern: &[u8], start_index: usize) -> Option<usize> {
         assert_ne!(pattern.len(), 0);
+
+        if start_index >= input.len() {
+            return None;
+        }
 
         if input.len() == 0 {
             return None;
         }
 
-        'outer: for i in 0..input.len() {
-            if input[i] == pattern[0] {
+        'outer: for i in start_index..input.len() {
+            if input[i] == pattern[0] || pattern[0] == b'.' {
                 for k in 1..pattern.len() {
                     if let Some(k_input) = input.get(i + k) {
-                        if *k_input != pattern[k] {
+                        if *k_input != pattern[k] && pattern[k] != b'.' {
                             continue 'outer;
                         }
                     } else {
@@ -100,7 +182,7 @@ impl Solution {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct PatternPair<'a> {
     pub fixed: Option<&'a [u8]>,
     pub repeatable: Option<&'a [u8]>,
@@ -199,6 +281,23 @@ mod test {
     }
 
     #[test]
+    fn capture_repeatable_4() {
+        let (pattern, rest) = Solution::capture_repeatable("a".as_bytes());
+
+        assert_eq!(true, pattern.is_none());
+        assert_eq!(rest, "a".as_bytes());
+    }
+
+    #[test]
+    fn capture_repeatable_5() {
+        let (pattern, rest) = Solution::capture_repeatable(".*c".as_bytes());
+
+        assert_eq!(false, pattern.is_none());
+        assert_eq!(pattern.unwrap(), ".*".as_bytes());
+        assert_eq!(rest, "c".as_bytes());
+    }
+
+    #[test]
     fn parse_regex() {
         let parsed = Solution::parse_regex("abc*d*efg*h*jk".as_bytes());
 
@@ -212,6 +311,20 @@ mod test {
             );
             node = &n.next_pattern;
         }
+    }
+
+    #[test]
+    fn parse_regex_only_fixed() {
+        let parsed = Solution::parse_regex("a".as_bytes());
+
+        assert_eq!(true, parsed.is_some());
+
+        let regex = parsed.unwrap();
+
+        assert_eq!(None, regex.repeatable);
+        assert_ne!(None, regex.fixed);
+        assert_eq!(b"a", regex.fixed.unwrap());
+        assert_eq!(true, regex.next_pattern.is_none());
     }
 
     #[test]
@@ -263,9 +376,48 @@ mod test {
     }
 
     #[test]
+    fn parse_regex_001() {
+        let parsed = Solution::parse_regex(".*c".as_bytes());
+        assert_eq!(true, parsed.is_some());
+
+        let regex = parsed.unwrap();
+
+        assert_eq!(true, regex.repeatable.is_some());
+        assert_eq!(true, regex.fixed.is_some());
+        assert_eq!(true, regex.next_pattern.is_none());
+
+        assert_eq!(b".*", regex.repeatable.unwrap());
+        assert_eq!(b"c", regex.fixed.unwrap());
+    }
+
+    #[test]
+    fn match_repeatable_simple() {
+        assert_eq!(
+            Solution::match_repeatable("abdabeabc".as_bytes(), ".*".as_bytes()),
+            true
+        );
+    }
+
+    #[test]
+    fn match_repeatable_fixed_simple() {
+        assert_eq!(
+            Solution::match_repeatable("aaabbb".as_bytes(), "a*b*".as_bytes()),
+            true
+        );
+    }
+
+    #[test]
+    fn match_repeatable_fixed_simple_not_match() {
+        assert_eq!(
+            Solution::match_repeatable("bbbaaa".as_bytes(), "a*b*".as_bytes()),
+            false
+        );
+    }
+
+    #[test]
     fn index_of_1() {
         assert_eq!(
-            Solution::index_of("abcd".as_bytes(), "bc".as_bytes()),
+            Solution::index_of_pattern("abcd".as_bytes(), "bc".as_bytes(), 0),
             Some(1)
         );
     }
@@ -273,7 +425,7 @@ mod test {
     #[test]
     fn index_of_2() {
         assert_eq!(
-            Solution::index_of("abdabeabc".as_bytes(), "abc".as_bytes()),
+            Solution::index_of_pattern("abdabeabc".as_bytes(), "abc".as_bytes(), 0),
             Some(6)
         );
     }
@@ -281,7 +433,7 @@ mod test {
     #[test]
     fn index_of_3() {
         assert_eq!(
-            Solution::index_of("abc".as_bytes(), "abc".as_bytes()),
+            Solution::index_of_pattern("abc".as_bytes(), "abc".as_bytes(), 0),
             Some(0)
         );
     }
@@ -289,7 +441,23 @@ mod test {
     #[test]
     fn index_of_4() {
         assert_eq!(
-            Solution::index_of("abc".as_bytes(), "abcdef".as_bytes()),
+            Solution::index_of_pattern("abc".as_bytes(), "abcdef".as_bytes(), 0),
+            Some(0)
+        );
+    }
+
+    #[test]
+    fn index_of_pattern_1() {
+        assert_eq!(
+            Solution::index_of_pattern("abcd".as_bytes(), "b.".as_bytes(), 0),
+            Some(1)
+        );
+    }
+
+    #[test]
+    fn index_of_pattern_2() {
+        assert_eq!(
+            Solution::index_of_pattern("abcd".as_bytes(), "..".as_bytes(), 0),
             Some(0)
         );
     }
