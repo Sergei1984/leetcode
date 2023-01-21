ab7153769b7e3b7330e183e718567c32606d523f
Serhii Tokariev <seregat1984@gmail.com>
2023-01-21 15:58:12 +0200

    37. Sudoku solver: basic solving

--- src/p037_hard_sudoku_solver.rs
+++ src/p037_hard_sudoku_solver.rs
@@ -1,17 +1,49 @@
 #![allow(unused)]
 
+use std::{collections::HashSet, ops::Range};
+
 pub struct Solution;
 
 impl Solution {
     pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
         let mut sudoku = Sudoku::from_board(board);
-        sudoku.print();
+        sudoku.print(true);
+
+        let result = Self::try_solve(&mut sudoku);
+        if let Some(solved) = result {
+            solved.print(true);
+        }
+    }
+
+    fn try_solve(sudoku: &mut Sudoku) -> Option<&Sudoku> {
+        loop {
+            println!("==============STEP===============");
+            let next_pos = sudoku.find_cell_with_single_value();
+            if let Some((row, col, digit)) = next_pos {
+                sudoku.put(row, col, digit);
+                sudoku.print(true);
+            } else {
+                break;
+            }
+
+            println!("");
+        }
+
+        if sudoku.empty_cell_count() == 0 {
+            return Some(sudoku);
+        }
+
+        None
     }
 }
 
 #[derive(Default, Debug)]
 struct Sudoku {
     field: [[Option<u8>; 9]; 9],
+    empty_cell_count: u8,
+    row_missing_numbers: [HashSet<u8>; 9],
+    col_missing_numbers: [HashSet<u8>; 9],
+    quad_missing_numbers: [[HashSet<u8>; 3]; 3],
 }
 
 impl Sudoku {
@@ -22,21 +54,76 @@ impl Sudoku {
                 let digit = board[row][col];
 
                 result.field[row][col] = if digit == '.' {
+                    result.empty_cell_count += 1;
                     None
                 } else {
                     Some(digit.to_digit(10).unwrap() as u8)
                 };
             }
         }
+        result.init_missing_num_data();
 
         result
     }
 
-    pub fn print(&self) {
+    #[inline]
+    pub fn rows(&self) -> Range<usize> {
+        0..9
+    }
+
+    #[inline]
+    pub fn cols(&self) -> Range<usize> {
+        0..9
+    }
+
+    #[inline]
+    pub fn quad_rows(&self) -> Range<usize> {
+        0..3
+    }
+
+    #[inline]
+    pub fn quad_cols(&self) -> Range<usize> {
+        0..3
+    }
+
+    pub fn quad_cell_rows(&self, quad_row: usize, quad_col: usize) -> Range<usize> {
+        quad_row * 3..(quad_row * 3) + 3
+    }
+
+    pub fn quad_cell_cols(&self, quad_row: usize, quad_col: usize) -> Range<usize> {
+        quad_col * 3..(quad_col * 3) + 3
+    }
+
+    // Gets quad row and col by cell row and col
+    pub fn quad_coords(&self, row: usize, col: usize) -> (usize, usize) {
+        (row / 3, col / 3)
+    }
+
+    pub fn empty_cell_count(&self) -> u8 {
+        self.empty_cell_count
+    }
+
+    pub fn find_cell_with_single_value(&self) -> Option<(usize, usize, u8)> {
+        for row in self.rows() {
+            for col in self.cols() {
+                if self.field[row][col].is_none() {
+                    let possible_digits = self.cell_possible_digits(row, col);
+                    if possible_digits.len() == 1 {
+                        return Some((row, col, *possible_digits.iter().next().unwrap()));
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    pub fn print(&self, short: bool) {
+        println!("==============================");
+        println!("Empty cells: {}", self.empty_cell_count);
         println!("|-----------------------------|");
-        for row in 0..9 {
+        for row in self.rows() {
             print!("|");
-            for col in 0..9 {
+            for col in self.cols() {
                 let val = self.field[row][col];
                 if val.is_none() {
                     print!(" . ");
@@ -54,6 +141,107 @@ impl Sudoku {
                 println!("|-----------------------------|");
             }
         }
+
+        if short {
+            return;
+        }
+        println!("Row missings: ");
+        for row in self.rows() {
+            println!("R {}: {:?}", row, self.row_missing_numbers[row]);
+        }
+        println!("Col missings: ");
+        for col in self.cols() {
+            println!("C {}: {:?}", col, self.col_missing_numbers[col]);
+        }
+        println!("Quad missings:");
+        for quad_row in self.quad_rows() {
+            for quad_col in self.quad_cols() {
+                println!(
+                    "Q ({}, {}): {:?}",
+                    quad_row, quad_col, self.quad_missing_numbers[quad_row][quad_col]
+                );
+            }
+        }
+
+        println!("==============================");
+    }
+
+    pub fn put(&mut self, row: usize, col: usize, value: u8) -> bool {
+        self.field[row][col] = Some(value);
+        self.empty_cell_count -= 1;
+
+        let mut value_is_correct = true;
+
+        value_is_correct &= self.col_missing_numbers[col].remove(&value);
+        value_is_correct &= self.row_missing_numbers[row].remove(&value);
+
+        let (quad_row, quad_col) = self.quad_coords(row, col);
+        value_is_correct &= self.quad_missing_numbers[quad_row][quad_col].remove(&value);
+
+        value_is_correct
+    }
+
+    fn init_missing_num_data(&mut self) {
+        let full_digits = HashSet::<u8>::from([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        // rows
+        for row in self.rows() {
+            let mut row_digits = full_digits.clone();
+            for col in self.cols() {
+                let d = self.field[row][col];
+                if let Some(v) = d {
+                    row_digits.remove(&v);
+                }
+            }
+            self.row_missing_numbers[row] = row_digits;
+        }
+
+        // cols
+        for col in self.cols() {
+            let mut col_digits = full_digits.clone();
+            for row in self.rows() {
+                let d = self.field[row][col];
+                if let Some(v) = d {
+                    col_digits.remove(&v);
+                }
+            }
+            self.col_missing_numbers[col] = col_digits;
+        }
+
+        // quads
+        for quad_row in self.quad_rows() {
+            for quad_col in self.quad_cols() {
+                let mut quad_digits = full_digits.clone();
+
+                for row in self.quad_cell_rows(quad_row, quad_col) {
+                    for col in self.quad_cell_cols(quad_row, quad_col) {
+                        let d = self.field[row][col];
+                        if let Some(v) = d {
+                            quad_digits.remove(&v);
+                        }
+                    }
+                }
+
+                self.quad_missing_numbers[quad_row][quad_col] = quad_digits;
+            }
+        }
+    }
+
+    fn cell_possible_digits(&self, row: usize, col: usize) -> HashSet<u8> {
+        let missing_rows = &self.row_missing_numbers[row];
+        let missing_col = &self.col_missing_numbers[col];
+
+        let (quad_row, quad_col) = self.quad_coords(row, col);
+        let missing_quad = &self.quad_missing_numbers[quad_row][quad_col];
+
+        let missing_row_cols: HashSet<u8> =
+            missing_col.intersection(missing_col).map(|a| *a).collect();
+
+        let missing_digits: HashSet<u8> = missing_quad
+            .intersection(&missing_row_cols)
+            .map(|a| *a)
+            .collect();
+
+        missing_digits
     }
 }
 
