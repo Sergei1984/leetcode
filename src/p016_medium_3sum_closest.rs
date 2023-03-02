2d5c7385ec306662302dc9e7533666489995f41b
Serhii Tokariev <seregat1984@gmail.com>
2023-03-02 16:35:11 +0200

    [WIP] 16. 3Sum closest

--- src/p016_medium_3sum_closest.rs
+++ src/p016_medium_3sum_closest.rs
@@ -12,56 +12,94 @@ impl Solution {
 
         n.sort();
 
-        let mut global_min_diff = i32::MAX;
-        let mut global_closest_sum = i32::MAX;
+        let mut diff = i32::MAX;
+        let mut closest_sum = i32::MAX;
 
-        let mut min = 0;
-        let mut max = n.len() - 1;
+        for min in 0..n.len() - 1 {
+            for max in (min + 1..n.len()).rev() {
+                let reminder = n[min] + n[max];
 
-        loop {
-            if max - min <= 1 {
-                break;
-            }
+                let inner = &n[min + 1..max];
+                let start = inner
+                    .partition_point(|x| *x <= reminder)
+                    .min(if inner.len() == 0 { 0 } else { inner.len() - 1 });
 
-            let reminder = n[min] + n[max];
+                let mut end = if inner.len() == 0 { 0 } else { inner.len() - 1 };
+                for i in start..inner.len() {
+                    if inner[i] > reminder {
+                        end = i;
+                        break;
+                    }
+                }
 
-            let mut closest = i32::MAX;
-            let mut diff = i32::MAX;
+                for num in &inner[start..end] {
+                    let sum = reminder + *num;
 
-            let inner = &n[min + 1..max];
-            let idx = inner.partition_point(|x| *x < reminder);
+                    let current_diff = (sum - target).abs();
 
-            let start = if idx > 0 { idx - 1 } else { idx };
+                    if current_diff < diff {
+                        diff = current_diff;
+                        closest_sum = sum;
 
-            for num in &inner[start..(idx + 2).min(inner.len())] {
-                let sum = reminder + num;
-
-                let current_diff = (sum - target).abs();
-
-                if current_diff < diff {
-                    diff = current_diff;
-                    closest = sum;
-
-                    if diff == 0 {
-                        return sum;
+                        if diff == 0 {
+                            return sum;
+                        }
                     }
                 }
             }
-
-            if global_min_diff > diff {
-                global_min_diff = diff;
-                global_closest_sum = closest;
-            }
-
-            if closest > target {
-                max -= 1;
-            }
-            if closest < target {
-                min += 1;
-            }
         }
 
-        global_closest_sum
+        closest_sum
+    }
+
+    // [-5, -2, 0, 5, 7], 3
+    //     vvvv
+    // [-8, -5, 3, 2, 4]
+    //     vvvv
+    // (val=5, idx=3)
+    pub fn find_closest(n: &[i32], target: i32) -> Option<(i32, usize)> {
+        // let mapped: Vec<i32> = n.iter().map(|x| *x - target).collect();
+
+        if n.len() == 0 {
+            return None;
+        }
+
+        if n.len() == 1 {
+            return Some((n[0], 0));
+        }
+
+        let index = n.partition_point(|x| *x - target < 0);
+
+        // all items are less
+        if index == n.len() {
+            return Some((n[n.len() - 1], n.len() - 1));
+        }
+
+        if index == 0 {
+            return Some((n[0], 0));
+        }
+
+        let d = (n[index] - target).abs();
+
+        let d_next = n
+            .get(index + 1)
+            .map(|v| (*v - target).abs())
+            .unwrap_or(i32::MAX);
+
+        let d_prev = n
+            .get(index - 1)
+            .map(|v| (*v - target).abs())
+            .unwrap_or(i32::MAX);
+
+        if d < d_next {
+            if d < d_prev {
+                return Some((n[index], index));
+            } else {
+                return Some((n[index - 1], index - 1));
+            }
+        } else {
+            return Some((n[index + 1], index + 1));
+        }
     }
 }
 
@@ -89,6 +127,46 @@ mod test {
         assert_eq!(
             Solution::three_sum_closest(vec![4, 0, 5, -5, 3, 3, 0, -4, -5], -2),
             -2
+        );
+    }
+
+    #[test]
+    pub fn find_closest_001() {
+        assert_eq!(
+            Solution::find_closest(&vec![-3, -2, 0, 1, 10], 2),
+            Some((1, 3))
+        );
+    }
+
+    #[test]
+    pub fn find_closest_002() {
+        assert_eq!(
+            Solution::find_closest(&vec![-3, -2, 0, 1, 10], -3),
+            Some((-3, 0))
+        );
+    }
+
+    #[test]
+    pub fn find_closest_003() {
+        assert_eq!(
+            Solution::find_closest(&vec![-3, -2, 0, 1, 10], -100),
+            Some((-3, 0))
+        );
+    }
+
+    #[test]
+    pub fn find_closest_004() {
+        assert_eq!(
+            Solution::find_closest(&vec![-3, -2, 0, 1, 10], 100),
+            Some((10, 4))
+        );
+    }
+
+    #[test]
+    pub fn find_closest_005() {
+        assert_eq!(
+            Solution::find_closest(&vec![-3, -2, 0, 1, 10], 11),
+            Some((10, 4))
         );
     }
 }
